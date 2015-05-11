394fd3feb23b896092f0dd99ec79da98d007e50d
perttvil <vili.perttila@gmail.com>
2015-05-11 20:38:16 +0300

    finishing touch.

--- shinyapp/run.R
+++ shinyapp/run.R
@@ -1,5 +1,5 @@
 # If you encounter a "R session aborted" situation when calling runApp(), try
-# install.packages(c("Rcpp", "httpuv", "shiny"))
+# install.packages("shiny")
 # install_github('rstudio/shinyapps')
 library(shiny)
 runApp()
